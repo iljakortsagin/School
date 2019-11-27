6763ad2266f29b2337f7563bb753503925dc285e
pupil <pupil@ivkhk.ee>
2019-11-27 12:32:33 +0200

    School.java Change #1

--- src/school/School.java
+++ src/school/School.java
@@ -15,7 +15,7 @@ public class School {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+   
         // TODO code application logic here
         
         public static void main(String[] args) {
